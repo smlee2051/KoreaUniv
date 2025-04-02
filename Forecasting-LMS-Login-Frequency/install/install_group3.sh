851d7245fbbf43ec89ee7bf0878dd4d6f1f08980
smlee2051 <smlee2051@gmail.com>
2025-04-02 11:14:13 +0900

    Update install_group3.sh

--- Forecasting-LMS-Login-Frequency/install/install_group3.sh
+++ Forecasting-LMS-Login-Frequency/install/install_group3.sh
@@ -1,13 +1,22 @@
-torch==2.2.2
-accelerate==0.28.0
-einops==0.7.0
-matplotlib==3.7.0
-numpy==1.23.5
-pandas==1.5.3
-scikit_learn==1.2.2
-scipy==1.12.0
-tqdm==4.65.0
-peft==0.4.0
-transformers==4.31.0
-deepspeed==0.14.0
-sentencepiece==0.2.0
+#!/bin/bash
+
+# 1. Conda 환경 생성
+conda create -n time_llm python=3.11 -y
+conda activate time_llm
+
+# 2. pip 설치 패키지
+pip install torch==2.2.2
+pip install accelerate==0.28.0
+pip install einops==0.7.0
+pip install matplotlib==3.7.0
+pip install numpy==1.23.5
+pip install pandas==1.5.3
+pip install scikit_learn==1.2.2
+pip install scipy==1.12.0
+pip install tqdm==4.65.0
+pip install peft==0.4.0
+pip install transformers==4.31.0
+pip install deepspeed==0.14.0
+pip install sentencepiece==0.2.0
+
+conda deactivate
