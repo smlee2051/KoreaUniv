c9b5309146ac1d2f1e46c83888b7babba0090935
smlee2051 <smlee2051@gmail.com>
2025-04-02 11:14:46 +0900

    Update install_group1.sh

--- Forecasting-LMS-Login-Frequency/install/install_group1.sh
+++ Forecasting-LMS-Login-Frequency/install/install_group1.sh
@@ -5,7 +5,6 @@ conda activate models_group1
 # Install core libraries
 conda install -c conda-forge matplotlib numpy pandas scikit-learn -y
 
-# Install PyTorch (CPU 버전)
 pip install torch==1.9.0
 
 # Install additional requirements
